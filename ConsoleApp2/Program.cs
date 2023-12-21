3ca94c96538903a06e2417d62920ce7f72094780
Никита <153456515+Lairont@users.noreply.github.com>
2023-12-22 00:07:18 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2
+            //    Console.Write("Первое число: ");
+            //    int x = int.Parse(Console.ReadLine());
+
+            //    Console.Write("Второе число: ");
+            //    int y = int.Parse(Console.ReadLine());
+
+            //    if (x % 2==0&&y%2==0)
+            //    {
+            //        x /= 2;
+            //        y /= 2;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+            //    else if (x%2==0||y%2==0)
+            //    {
+            //        x *= 2;
+            //        y *= 2;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+            //    else
+            //    {
+            //        x += 1;
+            //        y += 1;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+
+            //    Console.ReadLine();
+
+            //задание 3
+            //Console.Write("Введите количество баллов: ");
+            //int ball = int.Parse(Console.ReadLine());
+            //if (ball>=90&&ball<=100)
+            //{
+            //    Console.WriteLine("Отлично");
+            //}
+            //else if (ball>=70&&ball<=89)
+            //{
+            //    Console.WriteLine("Хорошо");
+            //}
+            //else if (ball>=50&&ball<=69)
+            //{
+            //    Console.WriteLine("Удовлетворительно");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неудовлетворительно");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
